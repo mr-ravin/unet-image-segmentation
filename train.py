2fb39e2263d047ca7f542211caba0102cec2bcc2
Ravin Kumar <16964978+mr-ravin@users.noreply.github.com>
2021-07-04 17:55:44 +0530

    Add files via upload

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ from model import UnetArchitecture
 TRAIN_IMAGEDIR_PATH = "dataset/train/images/" 
 TRAIN_MASKDIR_PATH = "dataset/train/masks/"
 VAL_IMAGEDIR_PATH = "dataset/validate/images/"
-VAL_MASKDIR_PATH = "dataset/validate/masks/"
+VAL_MASKDIR_PATH =  "dataset/validate/masks/"
 
 IMAGE_HT = 160
 IMAGE_WT = 240
@@ -22,3 +22,22 @@ BATCH_SIZE = 16
 EPOCHS = 100
 LR = 10.0**-4     # learning rate is set to 0.0001
 
+def train(loader,model,optimizer,loss_fn,scaler):
+  loop = tqdm(loader)
+  for batch_idx, (input_data,targets) in enumerate(loop):
+    input_data = input_data.to(device=DEVICE) # shift to GPU if enabled
+    targets = targets.float().unsqueeze().to(device=DEVICE) #shift to GPU if enabled.
+    
+    #  precision for GPU operations to improve performance while maintaining accuracy.
+    with torch.cud.amp.autocast():
+      predictions = model(input_data)
+      loss = loss_fn(predictions,targets)
+      
+    # backprop
+    optimizer.zero_grad()
+    scaler.scale(loss).backward() # gradient scaling
+    scaler.step(optimizer)
+    scaler.update()
+    
+    #update tqdm values
+    loop.set_postfix(loss=loss.item())
